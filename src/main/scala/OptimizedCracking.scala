6c7082447dcca399d1b3beb3d0b7acf93eba43d8
Peter Brumbach <contact@peterbrumbach.com>
2026-04-13 17:44:05 -0400

    Base done

--- src/main/scala/OptimizedCracking.scala
+++ src/main/scala/OptimizedCracking.scala
@@ -10,12 +10,17 @@ val numCores: Int = Runtime.getRuntime.availableProcessors() - 1
 val executorService: ExecutorService = Executors.newFixedThreadPool(numCores)
 implicit val executionContext: ExecutionContext = ExecutionContext.fromExecutor(executorService)
 
-val hashHashMap: Set[String] = null
-val charList: List[String] = null
+val hashHashMap: Set[String] = getHashes
+val charList: List[String] = getCharSet
+val charMax: Int = charList.size - 1
+val futureList: List[Future[String]] = List.fill(25)(Future.successful(""))
 
 @main def startUp(): Unit = {
-  println(io.Source.fromResource("hashes.txt"))
-  //print(charList)
+  var string = "a]"
+  for i <- 1 to 10 do {
+    string = incrementString(string, 23)
+    println(string)
+  }
 }
 
 def mainThread(): Unit = {
@@ -32,21 +37,22 @@ def checkingPasswords(startString: String): String = {
 }
 
 def incrementString(str: String, n: Int): String = {
-  ""
+  if str.isEmpty then return charList(n)
+  val currNum: Int = charList.indexOf(str(str.length-1).toString) + n
+  if currNum > charMax then incrementString(str.dropRight(1), currNum/charList.length)+charList(currNum % charList.length) else str.dropRight(1)+charList(currNum)
 }
 
 def findMatch(hash: String): String = if hashHashMap.contains(hash) then hash else ""
 
+// loads a set of password hashes from the provided hashes.txt file
 def sha256(text: String): String = {
   MessageDigest.getInstance("SHA-256")
     .digest(text.getBytes("UTF-8"))
     .map("%02x".format(_)).mkString.toUpperCase
 }
 
-// loads a set of password hashes from the provided hashes.txt file
 def getHashes: Set[String] = {
   val src = io.Source.fromResource("hashes.txt")
-  print("Hey\n")
   try {
     src.getLines().toSet
   } finally {
@@ -58,7 +64,7 @@ def getCharSet: List[String] = {
   val digits = "0123456789"
   val lowercase = "abcdefghijklmnopqrstuvwxyz"
   val uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  val symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+  val symbols: String = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
   val fullCharset = lowercase + uppercase + digits + symbols
-  "".split(fullCharset).toList
+  fullCharset.split("").toList
 }
