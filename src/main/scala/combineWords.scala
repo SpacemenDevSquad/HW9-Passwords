92f157fa752059232fcf7ecc7dcf0bc0cd1f9510
Peter Brumbach <contact@peterbrumbach.com>
2026-04-29 14:31:27 -0400

    SortedPasswords

---
+++ src/main/scala/combineWords.scala
@@ -0,0 +1,24 @@
+import java.security.MessageDigest
+import java.util.concurrent.{ExecutorService, Executors}
+import scala.annotation.tailrec
+import scala.collection.parallel.CollectionConverters.*
+import scala.collection.{AbstractIterable, AbstractIterator, mutable}
+import scala.concurrent.duration.*
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.{Failure, Success}
+import java.io.{FileWriter, PrintWriter}
+
+
+@main def combineWords(): Unit = {
+  val passwords: List[String] = getFileContents("words.txt").toList
+  for i <- passwords.indices do {
+    Future {
+      for j <- passwords.indices do {
+        val isMatch = findMatch(sha256(passwords(i)+passwords(j)))
+        if (isMatch != "") print("("+passwords(i)+passwords(j)+","+isMatch+")")
+      }
+    }
+  }
+}
+
+
